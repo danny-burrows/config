260ac503fccc726092ed6ee8b7a0cc9b9d6e60a5
Danny Burrows <danny.burrows@genomicsplc.com>
2022-08-05 16:49:22 +0100

    Adding fonts to deploy and fixing annoying dirname bug

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,9 @@ repo_dir_path=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd
 create_symlink_if_not_exists () {
     source=$1
     destination=$2
-
+    
     # Ensure the directory structure exists.
-    mkdir -p "$(dirname destination)"
+    mkdir -p $(bash -c "dirname $destination")
 
     # Attempt to create the symlink if it doesnt exist.
     if [ -f "$destination" ]; then
@@ -26,8 +26,15 @@ config_source="$repo_dir_path/.tmux.conf"
 config_dest="$HOME/.tmux.conf"
 create_symlink_if_not_exists "$config_source" "$config_dest"
 
+# Nvim config
 find "$repo_dir_path/.config" -type f -print0 | while IFS= read -r -d '' src; do
     dest="$HOME${src//$repo_dir_path/}"
     create_symlink_if_not_exists "$src" "$dest"
 done
 
+# Fonts
+#   NOTE: I think "~/.local/share/fonts" maybe only works on Ubuntu.
+find "$repo_dir_path/fonts" -type f -print0 | while IFS= read -r -d '' src; do
+    dest="$HOME/.local/share${src//$repo_dir_path/}"
+    create_symlink_if_not_exists "$src" "$dest"
+done
