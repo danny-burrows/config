49ddb9f0bbe1caa5e5a42d54ecd0205c43a921ed
Danny Burrows <danny.burrows@genomicsplc.com>
2022-08-05 18:21:59 +0100

    Adding git support in nvim

--- .config/nvim/lua/dannyb/packer.lua
+++ .config/nvim/lua/dannyb/packer.lua
@@ -4,9 +4,22 @@ return require("packer").startup(function(use)
     use { "wbthomason/packer.nvim" }
     use { "ellisonleao/gruvbox.nvim" }
 
+    -- Telescope search and grepping.
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
         requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    use {
+        'lewis6991/gitsigns.nvim', tag = 'release',
+       config = function()
+            require('gitsigns').setup {
+                signcolumn = false,
+                numhl = true,
+
+                current_line_blame = true,
+            }
+        end
     }
 
 end)
