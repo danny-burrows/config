1dcd2d3cce198893c6530f211a61c9878e2b4b37
Danny Burrows <danny.burrows@genomicsplc.com>
2022-08-05 16:11:32 +0100

    Appeasing shellcheck

--- deploy.sh
+++ deploy.sh
@@ -9,12 +9,12 @@ create_symlink_if_not_exists () {
     destination=$2
 
     # Ensure the directory structure exists.
-    mkdir -p $(dirname destination)
+    mkdir -p "$(dirname destination)"
 
     # Attempt to create the symlink if it doesnt exist.
     if [ -f "$destination" ]; then
         echo "Skipping: $destination already exists."
-    elif ln -s $source $destination; then
+    elif ln -s "$source" "$destination"; then
         echo "Symlink $destination created!"
     else
         echo "Error creating symlink"
@@ -24,11 +24,10 @@ create_symlink_if_not_exists () {
 # Tmux config
 config_source="$repo_dir_path/.tmux.conf"
 config_dest="$HOME/.tmux.conf"
-create_symlink_if_not_exists $config_source $config_dest
+create_symlink_if_not_exists "$config_source" "$config_dest"
 
-for FILE in $(find "$repo_dir_path/.config" -type f); do
-    src=$FILE
-    dest="$HOME$(sed "s#$repo_dir_path##g" <<< $FILE)"
-
-    create_symlink_if_not_exists $src $dest
+find "$repo_dir_path/.config" -type f -print0 | while IFS= read -r -d '' src; do
+    dest="$HOME${src//$repo_dir_path/}"
+    create_symlink_if_not_exists "$src" "$dest"
 done
+
