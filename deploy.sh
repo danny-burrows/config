a8195a65dc5b85abb483ba1eb786cd0447bdb6f4
Danny Burrows <danny.burrows@genomicsplc.com>
2022-08-05 11:00:10 +0100

    Using function for symlink creation

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,22 @@ set -x
 
 repo_dir_path=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+create_symlink_if_not_exists () {
+    source=$1
+    destination=$2
+
+    if [ -f "$destination" ]; then
+        echo "Skipping: $destination already exists."
+    elif ln -s $source $destination; then
+        echo "Symlink $destination created!"
+    else
+        echo "Error creating symlink"
+    fi
+}
+
 # Tmux config
+config_source="$repo_dir_path/.tmux.conf"
 config_dest="$HOME/.tmux.conf"
-if [ -f "$config_dest" ]; then
-    echo "Skipping: $config_dest already exists."
-elif ln -s $repo_dir_path/.tmux.conf $config_dest; then
-    echo "Symlink $config_dest created!"
-else
-    echo "Error creating symlink"
-fi
+create_symlink_if_not_exists $config_source $config_dest
+
 
