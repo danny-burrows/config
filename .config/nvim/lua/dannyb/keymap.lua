bffc1cb4c117b7057107d0c74bd6329c62f6495b
Danny Burrows <danny.burrows@genomicsplc.com>
2022-08-08 15:50:24 +0100

    Fixing to actually use the clipboard buffer

--- .config/nvim/lua/dannyb/keymap.lua
+++ .config/nvim/lua/dannyb/keymap.lua
@@ -5,9 +5,9 @@ vim.keymap.set("n", "<Leader>fh", ":Telescope find_files hidden=true<CR>")
 vim.keymap.set("n", "<Leader>g", ":Telescope live_grep<CR>")
 
 -- Copying and pasting to clipboard
-vim.keymap.set("n", "<Leader>y", "\"*yy")
-vim.keymap.set("v", "<Leader>y", "\"*y")
-vim.keymap.set("n", "<Leader>p", "\"*p")
+vim.keymap.set("n", "<Leader>y", "\"+yy")
+vim.keymap.set("v", "<Leader>y", "\"+y")
+vim.keymap.set("n", "<Leader>p", "\"+p")
 
 vim.keymap.set("n", "<Leader>e", ":Ex<CR>")
 
