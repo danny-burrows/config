2920b8c4ac500ea27d31b182630f68f1b15cf9f6
Danny Burrows <danny.burrows@genomicsplc.com>
2022-08-08 15:45:36 +0100

    Copying and pasting to clipboard

--- .config/nvim/lua/dannyb/keymap.lua
+++ .config/nvim/lua/dannyb/keymap.lua
@@ -1,8 +1,13 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<Leader>f", ":Telescope find_files<CR>")
+vim.keymap.set("n", "<Leader>ff", ":Telescope find_files<CR>")
 vim.keymap.set("n", "<Leader>fh", ":Telescope find_files hidden=true<CR>")
 vim.keymap.set("n", "<Leader>g", ":Telescope live_grep<CR>")
+
+-- Copying and pasting to clipboard
+vim.keymap.set("n", "<Leader>y", "\"*yy")
+vim.keymap.set("v", "<Leader>y", "\"*y")
+vim.keymap.set("n", "<Leader>p", "\"*p")
 
 vim.keymap.set("n", "<Leader>e", ":Ex<CR>")
 
