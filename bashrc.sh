e0f0404701c4193203c3c74448ae64d3cb2256a2
Danny Burrows <danny.burrows@genomicsplc.com>
2022-08-25 15:32:43 +0100

    Adding bashrc config

---
+++ bashrc.sh
@@ -0,0 +1,16 @@
+# dannys-custom-config
+
+alias vim=nvim
+
+alias v=vim
+alias g=git
+alias gs=git status
+
+# Start tmux if not already open
+if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux         ]] && [ -z "$TMUX" ]; then
+  exec tmux
+fi
+
+# Dont build squshed images
+export CICD_DOCKER_BUILD_NO_SQUASH=1
+
--- deploy.sh
+++ deploy.sh
@@ -20,22 +20,33 @@ create_symlink_if_not_exists () {
 }
 
 # Tmux config
+printf "Tmux Config\n"
 config_source="$repo_dir_path/.tmux.conf"
 config_dest="$HOME/.tmux.conf"
 create_symlink_if_not_exists "$config_source" "$config_dest"
 
 # Nvim config
+printf "\nNvim Config\n"
 find "$repo_dir_path/.config" -type f -print0 | while IFS= read -r -d '' src; do
     dest="$HOME${src//$repo_dir_path/}"
     create_symlink_if_not_exists "$src" "$dest"
 done
 
 # Fonts
+printf "\nFonts\n"
 find "$repo_dir_path/fonts" -type f -print0 | while IFS= read -r -d '' src; do
     dest="$HOME/.local/share${src//$repo_dir_path/}"
     create_symlink_if_not_exists "$src" "$dest"
 done
 
-echo "Bootstrapping Packer Config"
+printf "\nBootstrapping Packer Config\n"
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+
+printf "\nConfig .bashrc\n"
+BASHRC="$HOME/.bashrc"
+if $(grep -qxF '# dannys-custom-config' $BASHRC); then
+    echo "Skipping: Found config settings."
+else
+    cat "$repo_dir_path/bashrc.sh" >> $BASHRC
+fi
 
