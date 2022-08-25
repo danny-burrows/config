e45c2d10495636170a724f794fef387c2415cdee
Danny Burrows <danny.burrows@genomicsplc.com>
2022-08-25 15:56:04 +0100

    Adding some more work config to bashrc

--- bashrc.sh
+++ bashrc.sh
@@ -11,6 +11,10 @@ if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] &&
   exec tmux
 fi
 
+# Work Stuff
+
 # Dont build squshed images
 export CICD_DOCKER_BUILD_NO_SQUASH=1
+
+alias dev='ssh gcc-dev'
 
